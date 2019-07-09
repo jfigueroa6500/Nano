7068d02d6cee4352c4f58a44f8a8fd70d06739d4
jfigueroa6500 <figueroagsr@gmail.com>
2019-07-09 14:43:32 -0700

    Add files via upload

---
+++ helper/ImageProcessing.py
@@ -0,0 +1,65 @@
+
+#imports
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torchvision
+from torchvision import transforms
+from PIL import Image
+
+
+#Defines Images procession function
+def process_image(image):
+
+    #pre-processing image
+    image = Image.open(image)
+# Resize the images where shortest side is 256 pixels, keeping aspect ratio.
+    if image.width > image.height:
+
+        factor = image.width/image.height
+        image = image.resize(size=(int(round(factor*256,0)),256))
+    else:
+        factor = image.height/image.width
+        image = image.resize(size=(256, int(round(factor*256,0))))
+# Crop out the center 224x224 portion of the image.
+
+    image = image.crop(box=((image.width/2)-112, (image.height/2)-112, (image.width/2)+112, (image.height/2)+112))
+
+    # Convert to numpy array
+    np_image = np.array(image)
+    np_image = np_image/255
+    # Normalize image
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    np_image = (np_image - mean) / std
+# Reorder dimension for PyTorch
+    np_image = np.transpose(np_image, (2, 0, 1))
+
+    return np_image , data_directory
+
+
+def imshow(image, ax=None, title=None):
+
+    if ax is None:
+        fig, ax = plt.subplots()
+
+    # PyTorch tensors assume the color channel is the first dimension
+    # but matplotlib assumes is the third dimension
+    image = image.transpose((1, 2, 0))
+
+    # Undo preprocessing
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    image = std * image + mean
+
+
+    def imshow(image, ax=None, title=None):
+        if ax is None:
+            fig, ax = plt.subplots()
+
+    # Image needs to be clipped between 0 and 1 or it looks like noise when displayed
+    image = np.clip(image, 0, 1)
+
+    ax.imshow(image)
+
+    return ax
